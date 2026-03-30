d927766c52ef45668c87fbe9ff763dae7e0e46d0
Shirasaka Koume <grub_49@163.com>
2026-03-30 10:56:38 +0800

    feat: change waiting seconds

--- FuzzerFork.cpp
+++ FuzzerFork.cpp
@@ -110,8 +110,8 @@ struct GlobalEnv {
   FuzzJob *CreateNewJob(size_t JobId, GlobalCorpusInfo *GlobalCorpus, std::vector<TracePC::CoverageInfo> *CoverageInfos, ArgsInfo *AllArgsInfo) {
 
     while (!Ready()) {
-      Printf("HFC is not ready, wait for 1 second\n");
-      std::this_thread::sleep_for(std::chrono::seconds(1));
+      Printf("HFC is not ready, wait for 5 seconds\n");
+      std::this_thread::sleep_for(std::chrono::seconds(5));
     }
 
     // COV or Crash
